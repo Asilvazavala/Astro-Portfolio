0bf6297cceb70789a53d03befde135eaee438fa1
Asilvazavala <asilvazavala@gmail.com>
2024-02-21 13:37:52 -0600

    added configuration language english

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
